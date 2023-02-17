d951a49961a15145ddc5275f91e9286eb886b5cf
Darya <kopteleva@inbox.ru>
2023-02-17 21:35:26 +0300

    добавлено решение дз к семинару №1

--- homework1.py
+++ homework1.py
@@ -5,11 +5,11 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) |
 
-# number = input('Введите трехзначное число: ')
-# sum_n = 0
-# for i in number:
-#     sum_n += int(i)
-# print(sum_n)
+number = input('Введите трехзначное число: ')
+sum_n = 0
+for i in number:
+    sum_n += int(i)
+print(sum_n)
 
 # Задача 4: Петя, Катя и Сережа делают из бумаги журавликов. 
 # Вместе они сделали S журавликов. Сколько журавликов сделал каждый ребенок, 
@@ -22,8 +22,8 @@
 # 24 -> 4  16  4
 # 60 -> 10  40  10
 
-# count = int(input('Введите количество журавликов: '))
-# print(f"Петя сделал {int((count / 3) / 2)}, Катя сделала {int((count / 3) * 2)}, Сережа сделал {int((count / 3) / 2)}")
+count = int(input('Введите количество журавликов: '))
+print(f"Петя сделал {int((count / 3) / 2)}, Катя сделала {int((count / 3) * 2)}, Сережа сделал {int((count / 3) / 2)}")
 
 # Задача 6: Вы пользуетесь общественным транспортом? 
 # Вероятно, вы расплачивались за проезд и получали билет с номером. 
@@ -36,21 +36,21 @@
 # 385916 -> yes
 # 123456 -> no
 
-# happy_ticket = input('Введите номер билетика: ')
-# first_part = happy_ticket[:3]
-# second_part = happy_ticket[3:]
-# first_sum = 0
-# second_sum = 0
+happy_ticket = input('Введите номер билетика: ')
+first_part = happy_ticket[:3]
+second_part = happy_ticket[3:]
+first_sum = 0
+second_sum = 0
 
-# for i in first_part:
-#     first_sum += int(i)
-# for i in second_part:
-#     second_sum += int(i)
+for i in first_part:
+    first_sum += int(i)
+for i in second_part:
+    second_sum += int(i)
 
-# if first_sum == second_sum:
-#     print('Билетик счастливый, поздравляю!')
-# else:
-#     print('Билетик, к сожалению, не счастливый :(')
+if first_sum == second_sum:
+    print('Билетик счастливый, поздравляю!')
+else:
+    print('Билетик, к сожалению, не счастливый :(')
 
 
 
@@ -63,11 +63,11 @@
 # 3 2 4 -> yes
 # 3 2 1 -> no
 
-# n = int(input('Введите длину шоколадки: '))
-# m = int(input('Введите ширину шоколадки: '))
-# k = int(input('Введите количество долек, которые нужно отломить: '))
+n = int(input('Введите длину шоколадки: '))
+m = int(input('Введите ширину шоколадки: '))
+k = int(input('Введите количество долек, которые нужно отломить: '))
 
-# if k % 2 == 0 and n * m > k:
-#     print('Yes')
-# else:
-#     print('No')
+if k % 2 == 0 and n * m > k:
+    print('Yes')
+else:
+    print('No')
